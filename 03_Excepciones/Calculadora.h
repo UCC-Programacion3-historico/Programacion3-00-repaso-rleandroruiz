3173cfe4ce671d8d2f8e036287876f9ac2e11058
rleandroruiz <leo1998.04.24@hotmail.com>
2017-08-09 10:40:35 -0300

    Se implemento el ejercicio 3

--- 03_Excepciones/Calculadora.h
+++ 03_Excepciones/Calculadora.h
@@ -2,7 +2,51 @@
 #define REPASO_CALCULADORA_H
 
 
+template <class T>
+
 class Calculadora {
+
+private:
+    T a;
+    T b;
+
+public:
+    T getA() const {
+        return a;
+    }
+
+    void setA(T a) {
+        Calculadora::a = a;
+    }
+
+    T getB() const {
+        return b;
+    }
+
+    void setB(T b) {
+        Calculadora::b = b;
+    }
+
+    T sumar(){
+        return a + b;
+    }
+
+    T restar(){
+        return a - b;
+    }
+
+    T dividir(){
+        if(b == 0)
+                throw 95;
+        return a / b;
+    }
+
+    T multiplicar(){
+        return a * b;
+    }
+
+
+
 
 };
 
